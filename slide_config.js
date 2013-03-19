736464e457eed8b40455ac3568568afecda66f39
vinnizworld <aryans.vinay@gmail.com>
2013-03-19 09:04:29 +0530

    Standardization

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Web development<br>Standardization',
+    subtitle: '',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
@@ -19,11 +19,8 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
+    name: 'Vinay Aggarwal',
+    company: 'Developer, Softway',
     github: 'http://github.com/you'
   }/*, {
     name: 'Second Name',
